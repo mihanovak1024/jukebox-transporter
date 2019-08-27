c3e9fa2c3337f0a5883292389e0b2a2e1a063cbd
miha.novak1024 <miha.novak1024@gmail.com>
2019-08-27 18:58:32 +0200

    Add simple test to BaseTest (mvn install fix)

--- src/test/java/jukebox/BaseTest.java
+++ src/test/java/jukebox/BaseTest.java
@@ -1,8 +1,18 @@
 package jukebox;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @RunWith(MockitoJUnitRunner.class)
 public class BaseTest {
+
+    @Test
+    public void base_test() {
+        assertThat(1, equalTo(1));
+    }
+
 }
