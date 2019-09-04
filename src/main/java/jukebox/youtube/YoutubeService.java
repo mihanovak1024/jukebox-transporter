93b4aea75ac32bd3ea3740d703c8e1706cac263b
miha.novak1024 <miha.novak1024@gmail.com>
2019-09-04 20:15:39 +0200

    Add browser user-agent header

--- src/main/java/jukebox/youtube/YoutubeService.java
+++ src/main/java/jukebox/youtube/YoutubeService.java
@@ -2,10 +2,12 @@ package jukebox.youtube;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 interface YoutubeService {
 
+    @Headers("User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36")
     @GET("results")
     Call<String> getSearchResult(@Query("search_query") String searchQuery);
 
