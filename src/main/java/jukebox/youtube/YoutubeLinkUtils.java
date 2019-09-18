db1997f8fb4de82de471a604dfae096c180d4797
miha.novak1024 <miha.novak1024@gmail.com>
2019-09-18 18:24:09 +0200

    Change YoutubeUtils to Singleton + add list util method

--- src/main/java/jukebox/youtube/YoutubeLinkUtils.java
+++ src/main/java/jukebox/youtube/YoutubeLinkUtils.java
@@ -1,14 +1,26 @@
 package jukebox.youtube;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+// TODO: 2019-09-18 add tests
 public class YoutubeLinkUtils {
 
     private static final int VIDEO_ID_GROUP = 1;
     private static final Pattern VIDEO_ID_REGEX_PATTERN = Pattern.compile("watch.*video=([a-zA-Z0-9]+)");
 
-    private String getVideoIdFromLink(String link) throws YoutubeParserException {
+    private static YoutubeLinkUtils INSTANCE;
+
+    static YoutubeLinkUtils getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new YoutubeLinkUtils();
+        }
+        return INSTANCE;
+    }
+
+    String getVideoIdFromLink(String link) throws YoutubeParserException {
         Matcher matcher = VIDEO_ID_REGEX_PATTERN.matcher(link);
         if (matcher.find()) {
             return matcher.group(VIDEO_ID_GROUP);
@@ -16,7 +28,20 @@ public class YoutubeLinkUtils {
         throw new YoutubeParserException(YoutubeParserException.LINK_VIDEO_ID_PARSE_ERROR);
     }
 
-    private String createLinkFromVideoId(String videoId) {
+    String createLinkFromVideoId(String videoId) {
         return String.format(YoutubeConstants.VIDEO_URL_FORMAT, videoId);
+    }
+
+    List<String> transformVideoLinkListToVideoIdList(List<String> youtubeVideoLinks) {
+        List<String> youtubeVideoIds = new ArrayList<>();
+        for (String youtubeVideoLink : youtubeVideoLinks) {
+            try {
+                String youtubeVideoId = getVideoIdFromLink(youtubeVideoLink);
+                youtubeVideoIds.add(youtubeVideoId);
+            } catch (YoutubeParserException e) {
+                e.printStackTrace();
+            }
+        }
+        return youtubeVideoIds;
     }
 }
