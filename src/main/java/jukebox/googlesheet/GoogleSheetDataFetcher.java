91352ee73e8f66b39fab7ef9b313cf8dbaffbdeb
miha.novak1024 <miha.novak1024@gmail.com>
2019-09-22 14:05:07 +0200

    Refactor GoogleSheetDataFetcher API connection logic (GoogleSheetConnector)

--- src/main/java/jukebox/googlesheet/GoogleSheetDataFetcher.java
+++ src/main/java/jukebox/googlesheet/GoogleSheetDataFetcher.java
@@ -1,17 +1,6 @@
 package jukebox.googlesheet;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import jukebox.LocalProperties;
 import jukebox.Util;
@@ -20,25 +9,19 @@ import jukebox.network.NetworkDataCallback;
 import jukebox.network.NetworkDataFetcher;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 public class GoogleSheetDataFetcher implements NetworkDataFetcher<LocalProperties, List<GoogleSheetData>> {
-    private static final String APPLICATION_NAME = "JukeboxTransporter";
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
     private DataParser<GoogleSheetData, List> dataParser;
+    private GoogleSheetConnector googleSheetConnector;
 
-    public GoogleSheetDataFetcher(DataParser<GoogleSheetData, List> dataParser) {
+    public GoogleSheetDataFetcher(DataParser<GoogleSheetData, List> dataParser, GoogleSheetConnector googleSheetConnector) {
         this.dataParser = dataParser;
+        this.googleSheetConnector = googleSheetConnector;
     }
 
     public void fetchDataAsync(LocalProperties localProperties, NetworkDataCallback<List<GoogleSheetData>> callback, ExecutorService executorService) {
@@ -64,21 +47,28 @@ public class GoogleSheetDataFetcher implements NetworkDataFetcher<LocalPropertie
 
     }
 
+    /**
+     * Creates a request to Google Sheet API, parses the response with {@link GoogleSheetDataParser}
+     * and returns the list of parsed response data.
+     *
+     * @param localProperties
+     * @return list of parsed response data for each row of Google Sheet
+     * @throws GeneralSecurityException
+     * @throws IOException
+     */
     private List<GoogleSheetData> fetchGoogleSheetData(LocalProperties localProperties) throws GeneralSecurityException, IOException {
         List<GoogleSheetData> googleSheetDataList = null;
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         final String spreadsheetId = localProperties.getSpreadsheetId();
         final String range = localProperties.getSpreadsheetRange();
-        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-        ValueRange response = service.spreadsheets().values()
+
+        Sheets googleSheetService = googleSheetConnector.createConnection();
+        // Request to the GoogleSheet API
+        ValueRange googleSheetResponse = googleSheetService.spreadsheets().values()
                 .get(spreadsheetId, range)
                 .execute();
-        List<List<Object>> values = response.getValues();
-        if (values == null || values.isEmpty()) {
-            System.out.println("No data found.");
-        } else {
+
+        List<List<Object>> values = googleSheetResponse.getValues();
+        if (!Util.isNullOrEmpty(values)) {
             googleSheetDataList = new ArrayList<>();
             for (List row : values) {
                 try {
@@ -89,22 +79,10 @@ public class GoogleSheetDataFetcher implements NetworkDataFetcher<LocalPropertie
                     e.printStackTrace();
                 }
             }
+        } else {
+            System.out.println("No data found.");
         }
         return googleSheetDataList;
-    }
-
-    private Credential getCredentials(final NetHttpTransport netHttpTransport) throws IOException {
-        InputStream in = Util.readFromLocalResourceToInputStream(CREDENTIALS_FILE_PATH);
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                netHttpTransport, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
 }
